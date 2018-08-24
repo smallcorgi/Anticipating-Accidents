0b5695380a0f7b308aa47a3f27c2e8f239d04218
Fu-Hsiang Chan <fhchan@ambarella.com>
2018-08-23 20:25:01 -0700

    support tensorflow=1.6

--- accident.py
+++ accident.py
@@ -74,7 +74,7 @@ def build_model():
     }
 
     # Define a lstm cell with tensorflow
-    lstm_cell = tf.nn.rnn_cell.LSTMCell(n_hidden,n_hidden,initializer= tf.random_normal_initializer(mean=0.0,stddev=0.01),use_peepholes = True,state_is_tuple = False)
+    lstm_cell = tf.contrib.rnn.LSTMCell(n_hidden,initializer= tf.random_normal_initializer(mean=0.0,stddev=0.01),use_peepholes = True,state_is_tuple = False)
     # using dropout in output of LSTM
     lstm_cell_dropout = tf.nn.rnn_cell.DropoutWrapper(lstm_cell,output_keep_prob=1 - keep[0])
     # init LSTM parameters
@@ -86,6 +86,7 @@ def build_model():
     zeros_object = tf.to_float(tf.not_equal(tf.reduce_sum(tf.transpose(x[:,:,1:n_detection,:],[1,2,0,3]),3),0)) # frame x n x b
     # Start creat graph
     for i in range(n_frames):
+      with tf.variable_scope('model',reuse=tf.AUTO_REUSE):
         # input features (Faster-RCNN fc7)
         X = tf.transpose(x[:,i,:,:], [1, 0, 2])  # permute n_steps and batch_size (n x b x h)
         # frame embedded
@@ -94,21 +95,20 @@ def build_model():
         n_object = tf.reshape(X[1:n_detection,:,:], [-1, n_input]) # (n_steps*batch_size, n_input)
         n_object = tf.matmul(n_object, weights['em_obj']) + biases['em_obj'] # (n x b) x h
         n_object = tf.reshape(n_object,[n_detection-1,batch_size,n_att_hidden]) # n-1 x b x h
-        n_object = tf.mul(n_object,tf.expand_dims(zeros_object[i],2))
+        n_object = tf.multiply(n_object,tf.expand_dims(zeros_object[i],2))
 
         # object attention
         brcst_w = tf.tile(tf.expand_dims(weights['att_w'], 0), [n_detection-1,1,1]) # n x h x 1
-        image_part = tf.batch_matmul(n_object, tf.tile(tf.expand_dims(weights['att_ua'], 0), [n_detection-1,1,1])) + biases['att_ba'] # n x b x h
+        image_part = tf.matmul(n_object, tf.tile(tf.expand_dims(weights['att_ua'], 0), [n_detection-1,1,1])) + biases['att_ba'] # n x b x h
         e = tf.tanh(tf.matmul(h_prev,weights['att_wa'])+image_part) # n x b x h
         # the probability of each object
-        alphas = tf.mul(tf.nn.softmax(tf.reduce_sum(tf.batch_matmul(e,brcst_w),2),0),zeros_object[i])
+        alphas = tf.multiply(tf.nn.softmax(tf.reduce_sum(tf.matmul(e,brcst_w),2),0),zeros_object[i])
         # weighting sum
-        attention_list = tf.mul(tf.expand_dims(alphas,2),n_object)
+        attention_list = tf.multiply(tf.expand_dims(alphas,2),n_object)
         attention = tf.reduce_sum(attention_list,0) # b x h
         # concat frame & object
-        fusion = tf.concat(1,[image,attention])
-        # reuse variables
-        if i > 0 :  tf.get_variable_scope().reuse_variables()       
+        fusion = tf.concat([image,attention],1)
+
         with tf.variable_scope("LSTM") as vs:
             outputs,istate = lstm_cell_dropout(fusion,istate)
             lstm_variables = [v for v in tf.global_variables() if v.name.startswith(vs.name)]
@@ -122,16 +122,16 @@ def build_model():
             all_alphas = tf.expand_dims(alphas,0)
         else:
             temp_soft_pred = tf.reshape(tf.gather(tf.transpose(tf.nn.softmax(pred),(1,0)),1),(batch_size,1))
-            soft_pred = tf.concat(1,[soft_pred,temp_soft_pred])
+            soft_pred = tf.concat([soft_pred,temp_soft_pred],1)
             temp_alphas = tf.expand_dims(alphas,0)
-            all_alphas = tf.concat(0,[all_alphas, temp_alphas])
+            all_alphas = tf.concat([all_alphas, temp_alphas],0)
 
         # positive example (exp_loss)
-        pos_loss = -tf.mul(tf.exp(-(n_frames-i-1)/20.0),-tf.nn.softmax_cross_entropy_with_logits(pred, y))
+        pos_loss = -tf.multiply(tf.exp(-(n_frames-i-1)/20.0),-tf.nn.softmax_cross_entropy_with_logits(logits = pred, labels = y))
         # negative example
-        neg_loss = tf.nn.softmax_cross_entropy_with_logits(pred, y) # Softmax loss
+        neg_loss = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits = pred) # Softmax loss
 
-        temp_loss = tf.reduce_mean(tf.add(tf.mul(pos_loss,y[:,1]),tf.mul(neg_loss,y[:,0])))
+        temp_loss = tf.reduce_mean(tf.add(tf.multiply(pos_loss,y[:,1]),tf.multiply(neg_loss,y[:,0])))
         #loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
         loss = tf.add(loss, temp_loss)
         
@@ -168,6 +168,7 @@ def train():
              batch_ys = batch_data['labels']
              _,batch_loss = sess.run([optimizer,loss], feed_dict={x: batch_xs, y: batch_ys, keep: [0.5]})
              epoch_loss[batch-1] = batch_loss/batch_size
+             print "Epoch:", epoch+1, " done. Loss:", np.mean(epoch_loss)
          # print one epoch
          print "Epoch:", epoch+1, " done. Loss:", np.mean(epoch_loss)
          tStop_epoch = time.time()
